9229a7c669028e0b39a8e612e62600fce696b441
Malte Janssen <Malte Janssen>
2017-10-16 17:44:29 +0200

    Serial Interfaces added

---
+++ src/hal/io/Serial.cpp
@@ -0,0 +1,19 @@
+/*
+ * Serial.cpp
+ *
+ *  Created on: 16.10.2017
+ *      Author: aca311
+ */
+
+#include "Serial.h"
+
+namespace hal {
+namespace io {
+
+
+Serial::~Serial() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace io */
+} /* namespace hal */
