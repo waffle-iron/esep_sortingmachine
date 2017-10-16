9649747f1e61fe3eb79aa781bff693f73a3db200
Flo <Flo@MAC>
2017-10-16 12:03:51 +0200

    implement switchpoint open & close functionality

--- src/hal/actuators/SwitchPoint.cpp
+++ src/hal/actuators/SwitchPoint.cpp
@@ -6,19 +6,56 @@
  */
 
 #include "SwitchPoint.h"
+#include "GPIO.h"
 
 namespace hal {
-namespace actuators {
+	namespace actuators {
 
-SwitchPoint::SwitchPoint() {
-	// TODO Auto-generated constructor stub
+		SwitchPoint *SwitchPoint::_instance = nullptr;
 
-}
+		SwitchPoint *SwitchPoint::instance() {
+			if (_instance == nullptr) {
+				_instance = new Motor;
+			}
+			return _instance;
+		}
 
-SwitchPoint::~SwitchPoint() {
-	// TODO Auto-generated destructor stub
-}
+		SwitchPoint::SwitchPoint() {
+			// TODO Auto-generated constructor stub
 
-} /* namespace actuators */
+		}
+
+		SwitchPoint::~SwitchPoint() {
+			// TODO Auto-generated destructor stub
+		}
+
+		SwitchPoint::open(){
+
+			// 1001 0001 or
+			// 0010 0000
+			// 1010 0001
+
+			int bits = io::GPIO::instance()->read(PORT::B);
+			bits = bits | SP_OPEN;
+
+			io::GPIO::instance()->write(PORT::B, bits );
+		}
+
+		SwitchPoint::close(){
+
+			// 1011 0001 and
+			// 1101 1111
+			// 1001 0001
+
+			int bits = io::GPIO::instance()->read(PORT::B);
+			bits = bits & SP_OPEN;
+
+			io::GPIO::instance()->write(PORT::B, bits );
+		}
+
+
+
+
+	} /* namespace actuators */
 } /* namespace hal */
 
--- src/hal/actuators/SwitchPoint.h
+++ src/hal/actuators/SwitchPoint.h
@@ -8,13 +8,35 @@
 #ifndef SWITCHPOINT_H_
 #define SWITCHPOINT_H_
 
+/**
+ * Switchpoint open
+ */
+#define SP_OPEN 0b00100000;
+
+/**
+ * Switchpoint close
+ */
+#define SP_CLOSE 0b11011111;
+
 namespace hal {
 namespace actuators {
 
 class SwitchPoint {
 public:
+
 	static SwitchPoint *instance();
+
 	virtual ~SwitchPoint();
+
+	/**
+	 *  @brief opens switchpoint
+	 */
+	void open();
+
+	/**
+	 *  @brief closes switchpoint
+	 */
+	void close();
 
 private:
 	SwitchPoint();
