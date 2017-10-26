024554fb82f4a254fc3e5191e697f0223eaef71f
fabiansch <stahlschiff@web.de>
2017-10-26 17:20:37 +0200

    add LogicLayer

---
+++ src/logicLayer/LogicLayer.cpp
@@ -0,0 +1,21 @@
+/*
+ * LogicLayer.cpp
+ *
+ *  Created on: 26.10.2017
+ *      Author: abj240
+ */
+
+#include "LogicLayer.h"
+
+namespace logicLayer {
+
+LogicLayer::LogicLayer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LogicLayer::~LogicLayer() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace logicLayer */
---
+++ src/logicLayer/LogicLayer.h
@@ -0,0 +1,21 @@
+/*
+ * LogicLayer.h
+ *
+ *  Created on: 26.10.2017
+ *      Author: abj240
+ */
+
+#ifndef LOGICLAYER_H_
+#define LOGICLAYER_H_
+
+namespace logicLayer {
+
+class LogicLayer {
+public:
+	LogicLayer();
+	virtual ~LogicLayer();
+};
+
+} /* namespace logicLayer */
+
+#endif /* LOGICLAYER_H_ */
