5e09022a1e8a0b7998dc7a1f3b03caef31dc641e
fabiansch <stahlschiff@web.de>
2017-11-02 15:38:22 +0100

    refactor HAL and AsyncChannel - add comments and log errors

--- src/hal/io/AsyncChannel.cpp
+++ src/hal/io/AsyncChannel.cpp
@@ -22,37 +22,39 @@ AsyncChannel::AsyncChannel() {
 	//creating Channel
 	channelId = ChannelCreate_r(0);
 	if(channelId<0){
-		LOG_ERROR<<"Fail Channel create: "<<channelId;
+		LOG_ERROR<<"Channel create failed. Channel id: "<<channelId;
 		exit(EXIT_FAILURE);
 	}
 
 	//creating connection to Channel
 	connectionId = ConnectAttach_r(0, 0, channelId, 0, 0);
 	if(connectionId<0){
-		LOG_ERROR<<"Fail Channel create: "<<connectionId;
+		LOG_ERROR<<"Connection attach failed. Connection id: "<<connectionId;
 		exit(EXIT_FAILURE);
 	}
 
-	//LOG_HAL("Channel created: " + to_string(channelId) + " ( con: " + to_string(connectionId)+ ")");
+	LOG_DEBUG<<__FUNCTION__<<" channel created. Channel id: " + to_string(channelId) + " ( connection id: " + to_string(connectionId)+ ")"<<endl;
 }
 
 AsyncChannel::~AsyncChannel() {
 	LOG_SCOPE
+
 	ConnectDetach(connectionId);
 	ChannelDestroy(channelId);
 
-	//LOG_HAL("Channel destroyed");
+	LOG_DEBUG<<__FUNCTION__<<" channel destroyed."<<endl;;
 }
 
 AsyncMsg AsyncChannel::getNextMessage(){
 	LOG_SCOPE
+
 	AsyncMsg msg;
 	struct _pulse pulse;
 
 	//wait for next message
 	int ret = MsgReceivePulse_r(channelId, &pulse, sizeof(pulse), NULL);
 	if(ret < 0){
-		LOG_ERROR<<"Fail MsgReceivePulse: "<<ret;
+		LOG_ERROR<<__FUNCTION__<<" MsgReceivePulse failed. Error id: "<<ret<<endl;
 		exit(EXIT_FAILURE);
 	}
 
@@ -72,7 +74,7 @@ void AsyncChannel::sendMessage(AsyncMsg msg){
     	msg.type,
 		msg.value );
    	if(ret < 0){
-   		LOG_ERROR<<"Fail MsgReceivePulse: "<<ret;
+   		LOG_ERROR<<__FUNCTION__<<" MsgReceivePulse failed. Error id: "<<ret<<endl;
    		exit(EXIT_FAILURE);
    	}
 }
