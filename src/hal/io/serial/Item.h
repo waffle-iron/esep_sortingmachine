37323d6a5b92f46d69ef0a37e3ae9fae492a4abe
Flo <florian.heuer@haw-hamburg.de>
2017-11-06 12:40:01 +0100

    add Item structure

---
+++ src/hal/io/serial/Item.h
@@ -0,0 +1,20 @@
+/*
+ * Item.h
+ *
+ *  Created on: 06.11.2017
+ *      Author: Flo
+ */
+
+#ifndef ITEM_H_
+#define ITEM_H_
+
+#include "Signals.h"
+
+struct Item{
+	int dies;
+	int das;
+	int jenes;
+};
+
+
+#endif /* ITEM_H_ */
