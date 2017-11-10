72bb302ebd00da92047e4fd19e6801f858812547
robert <robert.palm@haw-hamburg.de>
2017-11-10 17:57:16 +0100

    signal mapping. serial incoming signals are now calling hardwareLayer functions

--- src/logicLayer/LogicLayer.cpp
+++ src/logicLayer/LogicLayer.cpp
@@ -28,21 +28,86 @@ void LogicLayer::notify(){
 	cout << "notify called";
 
 	while((signal = hal.getSignal()).name != Signalname::SIGNAL_BUFFER_EMPTY) {
-		switch(signal.name){
-			case Signalname::BLINK_GREEN_FAST:
-				hal.blinkGreen(Speed::fast);
+		cout << "LogicLayer: I have been notified! Now I go get the Signal."
+				<< endl;
+
+		switch (signal.name) {
+		// LIGHTS
+		case Signalname::YELLOW_LIGHT_ON:
+			hal.yellowLightOn();
 			break;
-			case Signalname::CONNECTION_LOST:
-				hal.blinkRed(Speed::slow);
+		case Signalname::RED_LIGHT_ON:
+			hal.redLightOn();
 			break;
-			case Signalname::CONNECTION_CONNECTED:
-				hal.redLightOff();
+		case Signalname::GREEN_LIGHT_ON:
+			hal.greenLightOn();
 			break;
-			default:
+		case Signalname::BLINK_GREEN_FAST:
+			hal.blinkGreen(Speed::fast);
+			break;
+		case Signalname::BLINK_RED_FAST:
+			hal.blinkRed(Speed::fast);
+			break;
+		case Signalname::BLINK_YELLOW_FAST:
+			hal.blinkYellow(Speed::fast);
+			break;
+		case Signalname::BLINK_GREEN_SLOW:
+			hal.blinkGreen(Speed::slow);
+			break;
+		case Signalname::BLINK_YELLOW_SLOW:
+			hal.blinkYellow(Speed::slow);
+			break;
+		case Signalname::BLINK_RED_SLOW:
+			hal.blinkRed(Speed::slow);
+			break;
+		case Signalname::RED_LIGHT_OFF:
+			hal.redLightOff();
+			break;
+		case Signalname::YELLOW_LIGHT_OFF:
+			hal.yellowLightOff();
+			break;
+		case Signalname::GREEN_LIGHT_OFF:
+			hal.greenLightOff();
+			break;
+
+			// MOTOR
+		case Signalname::MOTOR_START:
+			hal.motorStart();
+			break;
+		case Signalname::MOTOR_STOP:
+			hal.motorStop();
+			break;
+		case Signalname::MOTOR_FAST:
+			hal.motorFast();
+			break;
+		case Signalname::MOTOR_ROTATE_CLOCKWISE:
+			hal.motorRotateClockwise();
+			break;
+		case Signalname::MOTOR_ROTATE_COUNTER_CLOCKWISE:
+			hal.motorRotateCounterclockwise();
+			break;
+
+			// SWITCH
+		case Signalname::SWITCH_OPEN:
+			hal.switchPointOpen();
+			break;
+		case Signalname::SWITCH_CLOSE:
+			hal.switchPointClose();
+			break;
+
+
+		case Signalname::CONNECTION_LOST:
+			hal.blinkRed(Speed::slow);
+			break;
+		case Signalname::CONNECTION_CONNECTED:
+			hal.redLightOff();
+			break;
+		default:
 			break;
 		}
 
 	}
+	cout << "LogicLayer: I have been notified! All signals read." << endl;
 	//cout << hal.getSignal().name << endl;
 }
 } /* namespace logicLayer */
